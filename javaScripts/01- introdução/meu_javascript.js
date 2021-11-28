b417483fd1c15517a0202cd9575c3bb85303103b
Willian da Silva <williandasilvaoficial@gmail.com>
2021-11-28 00:26:08 -0300

    add conteúdo

---
+++ javaScripts/01- introdução/meu_javascript.js
@@ -0,0 +1,10 @@
+
+
+// comentário de uma linha
+
+/*
+    comentário de múltiplas linhas
+    Este tipo de comentário permite que várias linhas sejam comentadas 
+*/
+
+alert('Este é um teste de inclusão js á partir de um arquivo externo ')
