ffbc26c7879b188a420486e41484d25ca52e3598
Willian da Silva <williandasilvaoficial@gmail.com>
2021-12-04 19:06:48 -0300

    add conteudo

---
+++ javaScript ES6/Aula-01/condicional.js
@@ -0,0 +1,46 @@
+﻿/*  var nome = "Willian ";
+    var sobrenome = 'Silva';
+
+    alert(nome + sobrenome);
+*/
+
+    // condicional if/else
+
+/*
+        var idade = 20;
+
+    if(idade >= 18){
+        console.log('Você pode entrar na festa');
+    }
+    else{
+        console.log('Você é menor de 18 anos, portanto, não pode entra nafesta');
+    }
+*/
+
+/*
+    let hora = 19;
+
+    if(hora < 12){
+        console.log('Bom dia');
+    }
+    else if(hora <18){
+        console.log('Boa tarde');
+    }
+    else if(hora >18){
+        console.log('Boa noite');
+    }
+*/
+
+
+    let hora = prompt("Que horas são agora? ");
+
+    if(hora < 12){
+        console.log('Bom dia');
+    }
+    else if(hora > 12 && hora < 18){
+        console.log('Boa tarde');
+    }
+
+
+
+
