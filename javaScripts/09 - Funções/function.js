01a26b18c97f6c690f03b0d5175ede62e2c458ac
Willian da Silva <williandasilvaoficial@gmail.com>
2021-12-06 21:54:14 -0300

    add conteudo

---
+++ javaScripts/09 - Funções/function.js
@@ -0,0 +1,10 @@
+﻿        
+        // Ex01
+
+    function calculaAreaTerreno (largura, comprimento){
+        let area =largura * comprimento;
+
+        return area;
+    }
+    
+    console.log(calculaAreaTerreno(15, 25));
